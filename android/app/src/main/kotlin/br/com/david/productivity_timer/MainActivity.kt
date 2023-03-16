5cc8b7872eda2640913d88f2c38558b35bf05713
David Felix <davidffelix@hotmail.com>
2023-03-16 20:16:29 -0300

    Starting project

---
+++ android/app/src/main/kotlin/br/com/david/productivity_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.david.productivity_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Essa função vem do Material
+  // Passa a instância de uma classe widget para ele
+  runApp(MyApp());
+}
+
+class MyApp {
+  
+}
