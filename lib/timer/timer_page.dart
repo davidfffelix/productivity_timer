ae510f7e02bfcee222f9c57f5dcd5ab83b2402ab
David Felix <davidffelix@hotmail.com>
2023-03-17 12:01:13 -0300

    Insert buttons in the timer_page

--- lib/timer/timer_page.dart
+++ lib/timer/timer_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import '../components/productivity_button.dart';
 
 class TimerPage extends StatelessWidget {
   const TimerPage({super.key});
+
+  void emptyMethod() {}
+  final double defaultPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +14,55 @@ class TimerPage extends StatelessWidget {
         title: const Text('My Work Time'),
       ),
       body: Center(
-        child: Column(children: []),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: const Color(0xff009688),
+                    text: 'Work',
+                    size: 3,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: Color(0xff607D8B),
+                    //TODO: Formatar o texto
+                    text: 'Short Break',
+                    size: 3,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+                Expanded(
+                  child: ProductivityButton(
+                    color: Color(0xff455A64),
+                    text: 'Long Break',
+                    size: 3,
+                    onPressed: emptyMethod,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+              ],
+            ),
+            const Expanded(
+              child: Text('Hello!'),
+            ),
+            Row(children: []),
+          ],
+        ),
       ),
     );
   }
