6638818223f76c45a624761b0b423e5473835c53
David Felix <davidffelix@hotmail.com>
2023-03-23 08:53:20 -0300

    Update design button productivity_button

--- lib/components/productivity_button.dart
+++ lib/components/productivity_button.dart
@@ -17,31 +17,27 @@ class ProductivityButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        side: BorderSide(
-          width: 20,
-          color: Colors.orange,
+    return MaterialButton(
+      minWidth: size,
+      color: color,
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
         ),
       ),
-      onPressed: onPressed,
-      child: Text('Teste'),
     );
   }
 }
 
-// return Material(
-//       borderRadius: BorderRadius.circular(20),
-//       clipBehavior: Clip.antiAlias,
-//       child: MaterialButton(
-//         minWidth: size,
-//         color: color,
-//         onPressed: onPressed,
-//         child: Text(
-//           text,
-//           style: const TextStyle(
-//             color: Colors.white,
-//           ),
+
+// return ElevatedButton(
+//       onPressed: onPressed,
+//       child: Text(
+//         text,
+//         style: const TextStyle(
+//           color: Colors.white,
 //         ),
 //       ),
 //     );
--- lib/timer/timer_page.dart
+++ lib/timer/timer_page.dart
@@ -11,106 +11,112 @@ class TimerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff2D2D2D),
+      backgroundColor: const Color(0xff2D2D2D),
       appBar: AppBar(
         title: const Text('My Work Time'),
       ),
       // Widgets do tipo builder normalmente esperam como parâmetro uma função que retorna algo
       // Com o constraints conseguimos pegar a largura e altura disponível na tela. Nesse caso, somente o radius.
       // LayoutBuilder = Usamos ele para deixar a largura relativa ao tamanho da tela
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          // Pegamos a largura disponível pelo constraints
-          final double availableWidth = constraints.maxWidth;
-          return Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                  Expanded(
-                    child: ProductivityButton(
-                      color: const Color(0xffee4035),
-                      text: 'Work',
-                      size: 3,
-                      onPressed: emptyMethod,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          bottom: 20,
+        ),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            // Pegamos a largura disponível pelo constraints
+            final double availableWidth = constraints.maxWidth;
+            return Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                  Expanded(
-                    child: ProductivityButton(
-                      color: Color(0xff7bc043),
-                      //TODO: Formatar o texto
-                      text: 'Short Break',
-                      size: 3,
-                      onPressed: emptyMethod,
+                    Expanded(
+                      child: ProductivityButton(
+                        color: const Color(0xffee4035),
+                        text: 'Work',
+                        size: 3,
+                        onPressed: emptyMethod,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                  Expanded(
-                    child: ProductivityButton(
-                      color: Color(0xff0057e7),
-                      text: 'Long Break',
-                      size: 3,
-                      onPressed: emptyMethod,
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                ],
-              ),
-              // O const foi retirado por que o child (child: CircularPercentIndicator) não é constante
-              Expanded(
-                child: CircularPercentIndicator(
-                  radius: availableWidth / 2.5,
-                  lineWidth: 10,
-                  percent: 1,
-                  // Definimos widget dentro do indicador
-                  center: Text(
-                    '30:00',
-                    style: Theme.of(context).textTheme.displayMedium,
-                  ),
-                  progressColor: Color(0xff009688),
+                    Expanded(
+                      child: ProductivityButton(
+                        color: Color(0xff7bc043),
+                        //TODO: Formatar o texto
+                        text: 'Short Break',
+                        size: 3,
+                        onPressed: emptyMethod,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                      child: ProductivityButton(
+                        color: Color(0xff0057e7),
+                        text: 'Long Break',
+                        size: 3,
+                        onPressed: emptyMethod,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                  ],
                 ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                  Expanded(
-                    child: ProductivityButton(
-                      color: Color(0xff5e35b1),
-                      text: 'Stop',
-                      size: 3,
-                      onPressed: emptyMethod,
+                // O const foi retirado por que o child (child: CircularPercentIndicator) não é constante
+                Expanded(
+                  child: CircularPercentIndicator(
+                    radius: availableWidth / 2.5,
+                    lineWidth: 10,
+                    percent: 1,
+                    // Definimos widget dentro do indicador
+                    center: Text(
+                      '30:00',
+                      style: Theme.of(context).textTheme.displayMedium,
                     ),
+                    progressColor: Color(0xff009688),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                  Expanded(
-                    child: ProductivityButton(
-                      color: Color(0xff5e35b1),
-                      text: 'Restart',
-                      onPressed: emptyMethod,
-                      size: 3,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(defaultPadding),
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
+                    Expanded(
+                      child: ProductivityButton(
+                        color: Color(0xff5e35b1),
+                        text: 'Stop',
+                        size: 3,
+                        onPressed: emptyMethod,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                      child: ProductivityButton(
+                        color: Color(0xff5e35b1),
+                        text: 'Restart',
+                        size: 3,
+                        onPressed: emptyMethod,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
